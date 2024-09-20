fa882d532ed340e1b3cc96291a6a76a3d12fcf9b
Zulvani <zulvani.note@gmail.com>
2024-09-21 06:08:05 +0700

    initial commit

---
+++ src/main/java/com/zulvani/dss/DssCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.zulvani.dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zulvani/dss/DssCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zulvani.dss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
